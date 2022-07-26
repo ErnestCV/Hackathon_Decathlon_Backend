99120b5175acd5e870a9727852f45e0271e73c8c
ErnestCV <107429970+ErnestCV@users.noreply.github.com>
2022-07-26 12:09:23 +0200

    Project creation

---
+++ HackathonJuliolGrup3/src/main/java/com/ITAcademy/HackathonJuliol/HackathonJuliolGrup3/HackathonJuliolGrup3Application.java
@@ -0,0 +1,13 @@
+package com.ITAcademy.HackathonJuliol.HackathonJuliolGrup3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonJuliolGrup3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonJuliolGrup3Application.class, args);
+	}
+
+}
