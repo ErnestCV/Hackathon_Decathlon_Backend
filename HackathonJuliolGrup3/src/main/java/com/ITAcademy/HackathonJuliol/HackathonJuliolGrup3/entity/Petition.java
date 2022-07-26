ac349a84abbcec62129d8684a64f5c1e7e3f7900
Mangachh <mangachh@gmail.com>
2022-07-26 12:38:43 +0200

    recomendation & petition models created

--- HackathonJuliolGrup3/src/main/java/com/ITAcademy/HackathonJuliol/HackathonJuliolGrup3/entity/Petition.java
+++ HackathonJuliolGrup3/src/main/java/com/ITAcademy/HackathonJuliol/HackathonJuliolGrup3/entity/Petition.java
@@ -1,4 +1,29 @@
 package com.ITAcademy.HackathonJuliol.HackathonJuliolGrup3.entity;
 
+import java.util.List;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document("Petitions")
 public class Petition {
+    
+    private Long mongoId;
+    private String creatorId;
+    private String message;
+    // TODO: oju! in production create a class!
+    private List<String> tags;
+    private List<Recommendation> recomendations;
+    
+    @Builder.Default
+    private boolean isClosed = false;
+
 }
