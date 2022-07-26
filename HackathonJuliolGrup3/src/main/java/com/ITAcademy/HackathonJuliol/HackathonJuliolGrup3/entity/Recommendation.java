78b455c97e04b185f1db9c3032de7cdbce601304
Mangachh <mangachh@gmail.com>
2022-07-26 12:40:49 +0200

    recomendation without loombok.builder

--- HackathonJuliolGrup3/src/main/java/com/ITAcademy/HackathonJuliol/HackathonJuliolGrup3/entity/Recommendation.java
+++ HackathonJuliolGrup3/src/main/java/com/ITAcademy/HackathonJuliol/HackathonJuliolGrup3/entity/Recommendation.java
@@ -1,22 +1,23 @@
 package com.ITAcademy.HackathonJuliol.HackathonJuliolGrup3.entity;
+import java.util.ArrayList;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Builder;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class Recommendation {
 
     private Long recId;
     private String responderId;
+
     //TODO: oju! in production maybe class
-    @Builder.Default
-    private List<String> links = ArrayList<>();
-    @Builder.Default
+    private List<String> links = new ArrayList<>();
+
     private int likes = 0;
 }
